720cbd9ccc2087882922fa784a8995a0ab377a5f
ronnynaarala <88820095+ronnynaarala@users.noreply.github.com>
2021-08-26 09:34:56 +0000

    alot of script and css yes

--- proj.js
+++ proj.js
@@ -1,9 +1,23 @@
 function Virhe(){
-  if(document.getElementById('nimi').value==""){
-    console.log('nimikenttä on tyhjä');
+  if ((document.getElementById('nimi').value=="") || (document.getElementById('tieto').value=="")){
+    console.log('nimikentässä tai tietokentässä ei ollut tekstiä');
+    document.getElementById('tietonappi').className="b1f"
+    document.getElementById('tietonappi').textContent="Virhe! Et lisännyt tietoa."
+    document.getElementById('virhediv').className="virhe"
+    document.getElementById('virhediv').textContent="Täytä kaikki kentät"
+    setTimeout(function(){
+      document.getElementById('tietonappi').className="b1"
+      document.getElementById('tietonappi').textContent="Lisää tieto"
+      document.getElementById('virhediv').className=""
+      document.getElementById('virhediv').textContent=""
+    }, 3000);
   }
   else{
-    console.log('nimikentässä on tekstiä');
+    var nimi = document.getElementById('nimi').value
+    var tieto = document.getElementById('tieto').value
+    console.log('nimikentässä ja tietokentässä oli tekstiä');
+    document.getElementById('infodiv').className="infod"
+    document.getElementById('infodiv').textContent= nimi + ': ' + tieto
   }
   //console.log(document.getElementById('nimi').value);
 }
